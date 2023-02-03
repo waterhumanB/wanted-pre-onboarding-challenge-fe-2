48dc4e63b4ce1803dafc5b0b11d0fcebcfcacb4e
waterhumanB <dpfflsk45@gmail.com>
2023-02-03 14:17:23 +0900

    chore: 추가 셋팅 및 파일 구주화

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # 원티드 챌린지 TypeScript
+
+# 사칙연산 문서
+
+## [addition](global.html#addition)
+
+## [subtraction](global.html#subtraction)
+
+## [multiplication](global.html#multiplication)
---
+++ src/test.js
@@ -0,0 +1,7 @@
+/** @function multiplication
+ * @param {number} a 연산하고자 하는 0 이상의 정수
+ * @param {number} b 연산하고자 하는 0 이상의 정수
+ * @returns {number} a * b
+ */
+
+const multiplication = (a, b) => a * b;
